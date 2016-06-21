733ad8c4cd4bd89126f1cf8bc367a45870c4ad6f
Zassi12 <dzallinger@gmx.at>
2016-06-21 11:58:33 +0200

    LoginModel Props mit validierung angefangen

--- Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/LoginModel.cs
+++ Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/LoginModel.cs
@@ -7,5 +7,24 @@ namespace UI_Reiseboerse_Graf.Models
 {
     public class LoginModel
     {
+        private string email;
+
+        private string passwort;
+
+        public string Passwort
+        {
+            get { return passwort; }
+            set { passwort = value; }
+        }
+
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        
+
+        public int SessionId { get; set; }
     }
 }
