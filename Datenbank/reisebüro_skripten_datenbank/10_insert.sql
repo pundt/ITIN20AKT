7b391aa51af8a4078f6f1acc056f5cd90f770125
Marco Wurzenberger <marcowurzenberger@live.at>
2016-06-28 14:36:00 +0200

    Änderung am Batchjob und Hinzufügen von Inserts
    Änderung am Batchjob wegen Zugangsdaten, Dummy Inserts zum Testen
    hinzugefügt

---
+++ Datenbank/reisebüro_skripten_datenbank/10_insert.sql
@@ -0,0 +1,85 @@
+USE reisebuero;
+GO
+
+INSERT INTO Land(bezeichnung) VALUES('Österreich');
+INSERT INTO Land(bezeichnung) VALUES('Deutschland');
+INSERT INTO Land(bezeichnung) VALUES('Italien');
+INSERT INTO Land(bezeichnung) VALUES('Schweiz');
+INSERT INTO Land(bezeichnung) VALUES('Frankreich');
+
+INSERT INTO Ort(bezeichnung) VALUES('Wien');
+INSERT INTO Ort(bezeichnung) VALUES('Hamburg');
+INSERT INTO Ort(bezeichnung) VALUES('Rom');
+INSERT INTO Ort(bezeichnung) VALUES('Zürich');
+INSERT INTO Ort(bezeichnung) VALUES('Paris');
+
+INSERT INTO Adresse(land_id, ort_id, plz, strasse, nummer)
+VALUES(1, 1, 1010, 'Stephansplatz', '1');
+INSERT INTO Adresse(land_id, ort_id, plz, strasse, nummer)
+VALUES(2, 2, 22222, 'Riemerstraße', '2/2');
+INSERT INTO Adresse(land_id, ort_id, plz, strasse, nummer)
+VALUES(3, 3, 31113, 'Via Accia', '33');
+INSERT INTO Adresse(land_id, ort_id, plz, strasse, nummer)
+VALUES(4, 4, 40215, 'Bankenplatz', '4/Top 1');
+INSERT INTO Adresse(land_id, ort_id, plz, strasse, nummer)
+VALUES(5, 5, 51243, 'Plaza de Chateau', '5');
+
+INSERT INTO Benutzer(email, vorname, nachname, geschlecht, adresse_id, telefon)
+VALUES('muster@itfox.at', 'Max', 'Muster', 0, 1, 0043676123456);
+INSERT INTO Benutzer(email, vorname, nachname, geschlecht, adresse_id, telefon)
+VALUES('marco@itfox.at', 'Marco', 'Wurz', 0, 2, 0049743212121);
+INSERT INTO Benutzer(email, vorname, nachname, geschlecht, adresse_id, telefon)
+VALUES('claudia@itfox.at', 'Claudia', 'Stiegl', 1, 3, 003256124565);
+INSERT INTO Benutzer(email, vorname, nachname, geschlecht, adresse_id, telefon)
+VALUES('daniel@itfox.at', 'Daniel', 'Zalli', 0, 4, 00396123456);
+INSERT INTO Benutzer(email, vorname, nachname, geschlecht, adresse_id, telefon)
+VALUES('stefan@itfox.at', 'Stefan', 'Groig', 0, 5, 0055236458);
+
+INSERT INTO Kunde(benutzer_id, geburtsdatum, titel, land_id)
+VALUES(1, '1990/1/1', 'Mag.', 1);
+INSERT INTO Kunde(benutzer_id, geburtsdatum, titel, land_id)
+VALUES(2, '1989/3/8', '', 1);
+INSERT INTO Kunde(benutzer_id, geburtsdatum, titel, land_id)
+VALUES(4, '1991/12/12', 'Dr.', 3);
+
+INSERT INTO Mitarbeiter(benutzer_id, svnr) VALUES(3, 1234);
+INSERT INTO Mitarbeiter(benutzer_id, svnr) VALUES(5, 1470);
+
+INSERT INTO Verpflegung(bezeichnung) VALUES('Ohne Verpflegung');
+INSERT INTO Verpflegung(bezeichnung) VALUES('Frühstück');
+INSERT INTO Verpflegung(bezeichnung) VALUES('Halbpension');
+INSERT INTO Verpflegung(bezeichnung) VALUES('Vollpension');
+INSERT INTO Verpflegung(bezeichnung) VALUES('All Inclusive');
+
+INSERT INTO Unterkunft(bezeichnung, beschreibung, kategorie, verpflegung_id)
+VALUES('Hotel Arosa', 'Dieses Hotel bietet außergewöhnlichen Komfort', '4 Sterne', 4);
+INSERT INTO Unterkunft(bezeichnung, beschreibung, kategorie, verpflegung_id)
+VALUES('Hotel de Croissant', 'Dieses Hotel schmeckt nach Croissants', '3 Sterne', 2);
+INSERT INTO Unterkunft(bezeichnung, beschreibung, kategorie, verpflegung_id)
+VALUES('Pension Pomp', 'Luxus in seiner schönsten Form', '5 Sterne', 5);
+INSERT INTO Unterkunft(bezeichnung, beschreibung, kategorie, verpflegung_id)
+VALUES('Almhütte zum Sepp', 'Gute urige Küche und Jausen', '2 Sterne', 1);
+INSERT INTO Unterkunft(bezeichnung, beschreibung, kategorie, verpflegung_id)
+VALUES('Hotel Allin', 'Dieses Hotel bietet alles außer Vollpension', '4 Sterne', 3);
+
+INSERT INTO Reise(titel, beschreibung, unterkunft_id, preis_erwachsener, preis_kind)
+VALUES('Wandern in den Bergen', 'Erleben Sie die schöne Bergwelt in Österreich', 4, 65.99, 32.99);
+INSERT INTO Reise(titel, beschreibung, unterkunft_id, preis_erwachsener, preis_kind)
+VALUES('Wellnes pur', 'Genießen Sie italienische Luft und Wellnes vom Feinsten', 1, 150.00, 75.00);
+INSERT INTO Reise(titel, beschreibung, unterkunft_id, preis_erwachsener, preis_kind)
+VALUES('Fußball pur', 'Fahren Sie zum EM-Finale nach Paris!', 2, 199.99, 99.99);
+INSERT INTO Reise(titel, beschreibung, unterkunft_id, preis_erwachsener, preis_kind)
+VALUES('Urlaub der Creme de la Creme', 'Verbleiben Sie in einem unserer 4000 Zimmer', 3, 259.90, 129.90);
+INSERT INTO Reise(titel, beschreibung, unterkunft_id, preis_erwachsener, preis_kind)
+VALUES('Pokern wie die Pros', 'Fahren Sie zur World Pokers Tour nach Las Vegas!', 5, 1200.00, 600.00);
+
+INSERT INTO Reisedetail(reise_id, startdatum, enddatum, anmeldefrist)
+VALUES(1, '2016/07/31', '2016/08/06', '2016/06/30');
+INSERT INTO Reisedetail(reise_id, startdatum, enddatum, anmeldefrist)
+VALUES(2, '2016/12/01', '2016/12/06', '2016/11/01');
+INSERT INTO Reisedetail(reise_id, startdatum, enddatum, anmeldefrist)
+VALUES(3, '2016/07/25', '2016/07/30', '2016/06/15');
+INSERT INTO Reisedetail(reise_id, startdatum, enddatum, anmeldefrist)
+VALUES(4, '2017/01/07', '2017/01/21', '2016/12/23');
+INSERT INTO Reisedetail(reise_id, startdatum, enddatum, anmeldefrist)
+VALUES(5, '2017/03/01', '2017/03/15', '2017/02/02');
