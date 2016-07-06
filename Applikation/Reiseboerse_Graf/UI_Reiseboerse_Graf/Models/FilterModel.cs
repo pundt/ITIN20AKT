0a81271846b6e18e00a5d0932a3d64ecede5dac6
marcowurzenberger <marcowurzenberger@live.at>
2016-07-06 08:32:20 +0200

    Filtern: Models und View hinzugefügt
    Nochmal hinzugefügt, da trotz Commit und Sync gestern das Visual Studio
    nicht hochgeladen hat

--- Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/FilterModel.cs
+++ Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/FilterModel.cs
@@ -8,13 +8,52 @@ namespace UI_Reiseboerse_Graf.Models
 {
     public class FilterModel
     {
-        [Required()]
-        public int Kontinent_ID { get; set; }
-        public int Land_ID { get; set; }
-        public int Ort_ID { get; set; }
-        public List<int> Kategorien_ID { get; set; }
-        public int Preis_ID { get; set; }
-        
+
+        private decimal[,] preis = new decimal[3, 2] { { 0, 499 }, { 500, 1499 }, { 1500, 9999 } };
+
+        /// <summary>
+        /// Preis-Array für die Checkbox zur Auswahl
+        /// </summary>
+        public decimal[,] Preis
+        {
+            get { return preis = new decimal[3, 2] { { 0, 499 }, { 500, 1499 }, { 1500, 9999 } }; }
+        }
+
+        /// <summary>
+        /// Liste von LandModel für die DropDownListe
+        /// </summary>
+        public List<LandModel> Land { get; set; }
+
+        /// <summary>
+        /// Liste von OrtModel für die DropDownListe
+        /// </summary>
+        public List<OrtModel> Ort { get; set; }
+
+        /// <summary>
+        /// Hotelkategorie (Sterne) für Auswahl der Sterne (Checkbox)
+        /// </summary>
+        [Range(1, 5, ErrorMessage = "Nur 1 bis 5 zulässig!")]
+        public int HotelKategorie { get; set; }
+
+        /// <summary>
+        /// Liste für KategorieModel für DropDownListe (z.B. "Busreise")
+        /// </summary>
+        public List<KategorieModel> Kategorie { get; set; }
+
+        /// <summary>
+        /// Liste für VerpflegungModel für DropDownListe (z.B. "Halbpension")
+        /// </summary>
+        public List<VerpflegungModel> Verpflegung { get; set; }
+
+        /// <summary>
+        /// Zur Auswahl eines Startdatums
+        /// </summary>
+        public DateTime Startdatum { get; set; }
+
+        /// <summary>
+        /// Zur Auswahl eines Enddatums
+        /// </summary>
+        public DateTime Enddatum { get; set; }
     }
 
 
---
+++ Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/VerpflegungModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI_Reiseboerse_Graf.Models
+{
+    public class VerpflegungModel
+    {
+        /// <summary>
+        /// Id für die eindeutige Zuweisung
+        /// </summary>
+        public int Id { get; set; }
+
+        public string Bezeichnung { get; set; }
+    }
+}
