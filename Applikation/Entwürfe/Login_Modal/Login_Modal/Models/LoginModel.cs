90b61474101d5f7c21ec541b56583b85c67b43fa
claudia87 <claudia.stiglmayr@outlook.de>
2016-07-11 10:58:37 +0200

    Test für Login
    Login Fenster getestet in Sideproject

---
+++ Applikation/Entwürfe/Login_Modal/Login_Modal/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Login_Modal.Models
+{
+    public class LoginModel
+    {
+        [Required(AllowEmptyStrings =false,ErrorMessage ="*")]
+        [DataType(DataType.EmailAddress)]
+        public string Mail { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        public string Pwd { get; set; }
+    }
+}
