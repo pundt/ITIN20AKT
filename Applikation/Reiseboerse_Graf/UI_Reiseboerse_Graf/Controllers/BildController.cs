01a583a1923285dee0e545add6cfd89657180b2c
claudia87 <claudia.stiglmayr@outlook.de>
2016-08-16 20:04:39 +0200

    Bild auslesen aus der  DB und anzeigen (DB muss noch eingebunden werden)

---
+++ Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Controllers/BildController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI_Reiseboerse_Graf.Controllers
+{
+    public class BildController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Laden(int id)
+        {
+            string contentType = "image/jpeg";
+            byte[] data = BL_Reiseboerse_Graf.ReiseVerwaltung.Bildausleser(id);   //de facto - aus db laden / oder aus BL laden
+
+            return new FileContentResult(data, contentType);
+        }
+    }
+}
