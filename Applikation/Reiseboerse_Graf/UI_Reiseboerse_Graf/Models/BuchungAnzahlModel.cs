be90bd85d5a55a6728526105751228ed0eaf4205
claudia87 <claudia.stiglmayr@outlook.de>
2016-08-24 16:38:05 +0200

    Buchungen weiter gebastelt, aktuelle ReisedurchfuehrungsID ermittelt pro Buchung, Datenbankdaten müssen konsequent eingefügt werden damit testen funktioniert

--- Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/BuchungAnzahlModel.cs
+++ Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/BuchungAnzahlModel.cs
@@ -7,6 +7,8 @@ namespace UI_Reiseboerse_Graf.Models
 {
     public class BuchungAnzahlModel
     {
+        public int Reise_ID { get; set; }
+        public DateTime Beginndatum { get; set; }
         public int Anzahl_Erwachsene { get; set; }
         public int Anzahl_Kinder { get; set; }
         public decimal Preis_Erwachsene { get; set; }
