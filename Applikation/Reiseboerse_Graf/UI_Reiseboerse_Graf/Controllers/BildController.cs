c339266855ff98e29b0288a7db0a29618149a7fa
MichaelLennard <bullet-00@hotmail.com>
2016-08-25 07:59:03 +0200

    EigeneStyles geändert, AppConfig, BildController Auskommentiert, Hinzufuegen.cshtml Änderungen, bat.Datei Servername geändert

--- Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Controllers/BildController.cs
+++ Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Controllers/BildController.cs
@@ -26,11 +26,11 @@ namespace UI_Reiseboerse_Graf.Controllers
         [AllowAnonymous]
         public ActionResult Laden(int id)
         {
-            //string contentType = "image/jpeg";
-            //byte[] bilddaten=BildVerwaltung.LadeBild(id).Bilddaten;
+            string contentType = "image/jpeg";
+            byte[] bilddaten = BildVerwaltung.LadeBild(id).Bilddaten;
 
-            //return new FileContentResult(bilddaten, contentType);
-            return null;
+            return new FileContentResult(bilddaten, contentType);
+            
         }
     }
 }
