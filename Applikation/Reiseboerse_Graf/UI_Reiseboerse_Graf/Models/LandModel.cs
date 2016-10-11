6b43e65b072e7a0bd6b79022b43cb9682fc3b96e
marcowurzenberger <marcowurzenberger@live.at>
2016-10-11 08:51:40 +0200

    Filter-, Login-, Land- und OrtModel mit XML-Kommentaren versehen

--- Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/LandModel.cs
+++ Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/LandModel.cs
@@ -5,9 +5,19 @@ using System.Web;
 
 namespace UI_Reiseboerse_Graf.Models
 {
+    /// <summary>
+    /// Das Model, das den Namen eines Landes und die eindeutige ID beinhaltet
+    /// </summary>
     public class LandModel
     {
+        /// <summary>
+        /// Bezeichnung des Landes, z.B. "Deutschland"
+        /// </summary>
         public string landName { get; set; }
+
+        /// <summary>
+        /// Eindeutige ID des Landes, wird zur Zuweisung in DropDown-Listen verwendet
+        /// </summary>
         public int land_ID { get; set; }
     }
 }
--- Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/OrtModel.cs
+++ Applikation/Reiseboerse_Graf/UI_Reiseboerse_Graf/Models/OrtModel.cs
@@ -5,13 +5,19 @@ using System.Web;
 
 namespace UI_Reiseboerse_Graf.Models
 {
+    /// <summary>
+    /// Das Model, das den Namen eines Ortes und eine eindeutige ID beinhaltet
+    /// </summary>
     public class OrtModel
     {
         /// <summary>
-        /// Id für eindeutige Zuweisung
+        /// Eindeutige ID des Ortes, wird zur Zuweisung in DropDown-Listen verwendet
         /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Bezeichnung eines Ortes, z.B. "Wien"
+        /// </summary>
         public string Bezeichnung { get; set; }
     }
 }
